d8f3e0d235865acadafd7ebd19dbc8bf2b4fec22
Vinu Devaraj <vinudevaraj007@gmail.com>
2021-06-02 16:29:31 +0530

    card body added

--- src/components/ProjectCardComponent.js
+++ src/components/ProjectCardComponent.js
@@ -12,19 +12,19 @@ function ProjectCardComponent() {
           img={gotImg}
           txt1='custom api'
           txt2='got characters '
-          txt3='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, quaerat!'
+          txt3='this is a react project which uses a custom api create by me to return characters info'
         />
         <ShowCaseCard
           img={doctorImg}
           txt1='jwt auth'
           txt2='hospital app'
-          txt3='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, quaerat!'
+          txt3='this is my best project,this is a mern stack hospital project with jwt authentication '
         />
         <ShowCaseCard
           img={stockImg}
           txt1='material-ui'
           txt2='stock  tracker'
-          txt3='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, quaerat!'
+          txt3='this is a react project which helps us see the current stock market prices'
         />
       </div>
     </div>
