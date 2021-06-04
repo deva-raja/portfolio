d90f1f1f1bb37c5bf9be2eab4dcb843c715beb6d
Vinu Devaraj <vinudevaraj007@gmail.com>
2021-06-04 18:26:30 +0530

    contact form compleeted

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -16,28 +16,39 @@ function FormComponent() {
   });
 
   const onSubmit = (values, { setSubmitting }) => {
-    setSubmitting(true);
+    // setSubmitting(true);
     console.log(values);
-    setSubmitting(false);
+    // setSubmitting(false);
   };
+
+  const CustomInputComponent = ({ ...props }) => (
+    <textarea
+      className='textArea'
+      {...props}
+      rows='4'
+      cols='50'
+      placeholder='Enter message'
+    ></textarea>
+  );
+
   return (
     <div className='form-container'>
       <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={ContactSchema}>
         {(isSubmitting) => (
-          <Form>
-            <h2>Contact Me</h2>
-            <Field type='name' name='name' />
-            <ErrorMessage name='name' component='div' />
+          <Form className='form'>
+            <Field type='input' name='name' placeholder='Enter name' />
+            <ErrorMessage className='error' name='name' component='div' />
 
-            <Field type='email' name='email' />
-            <ErrorMessage name='email' component='div' />
+            <Field type='email' name='email' placeholder='Enter email' />
+            <ErrorMessage className='error' name='email' component='div' />
 
-            <Field type='message' name='message' />
-            <ErrorMessage name='message' component='div' />
+            {/* <Field type='message' name='message' placeholder='Enter message' /> */}
+            <Field name='message' as={CustomInputComponent} />
+            <ErrorMessage className='error' name='message' component='div' />
 
-            <button type='submit' disabled={isSubmitting}>
-              Submit
-            </button>
+            {/* <button type='submit' disabled={isSubmitting}> */}
+            <button type='submit'>SUBMIT</button>
+            <h2>call me at 7559961738</h2>
           </Form>
         )}
       </Formik>
--- src/components/FormImageComponent.js
+++ src/components/FormImageComponent.js
@@ -4,27 +4,28 @@ import { GiSevenPointedStar } from 'react-icons/gi';
 import { GiTriangleTarget } from 'react-icons/gi';
 import { GiPlainCircle } from 'react-icons/gi';
 import { VscDebugStop } from 'react-icons/vsc';
-import { GiCircle } from 'react-icons/gi';
 import { FiOctagon } from 'react-icons/fi';
 
 import Tilt from 'react-tilt';
 
 function FormImageComponent() {
   return (
-    <Tilt className='contact-image-container'>
-      <div className='contact-image-wrapper'>
-        <div>
-          <GiPlainCircle className='form-image-icons form-icon1' />
-          <VscDebugStop className='form-image-icons form-icon3' />
-          <GiTriangleTarget className='form-image-icons form-icon4' />
+    <>
+      <Tilt className='contact-image-container'>
+        <div className='contact-image-wrapper'>
+          <div>
+            <GiPlainCircle className='form-image-icons form-icon1' />
+            <VscDebugStop className='form-image-icons form-icon3' />
+            <GiTriangleTarget className='form-image-icons form-icon4' />
+          </div>
+          <img className='img' src={contactImg} alt='contact-me' />
+          <div>
+            <GiSevenPointedStar className='form-image-icons form-icon5' />
+            <FiOctagon className='form-image-icons form-icon2' />
+          </div>
         </div>
-        <img className='img' src={contactImg} alt='contact-me' />
-        <div>
-          <GiSevenPointedStar className='form-image-icons form-icon5' />
-          <FiOctagon className='form-image-icons form-icon2' />
-        </div>
-      </div>
-    </Tilt>
+      </Tilt>
+    </>
   );
 }
 
